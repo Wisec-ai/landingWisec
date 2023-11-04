287c4d855fe527f9186fbb1bd685e7557da02397
Kevin Gomez Lazaro <kevin94897@gmail.com>
2023-11-04 11:44:49 -0500

    Ultimos cambios

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}","./node_modules/flowbite/**/*.js"],
   theme: {
+    colors: {
+      primary: '#071c42',
+      secondary: '#3dcab1',
+      marked: '#80e704'
+    },
     extend: {
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'hero': "url('../src/assets/banner_background_cta.png')",
       },
     },
   },
