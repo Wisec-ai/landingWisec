89d84455efb2243ab19f76c75cf13e980e587680
kevin94897 <34484011+kevin94897@users.noreply.github.com>
2024-11-12 20:24:40 -0500

    Cambios Landing AICON

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}","./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
     colors: {
-      primary: '#071c42',
-      secondary: '#80e704',
-      marked: '#3dcab1',
-      constructor: '#ffd700'
+      primary: '#012130',
+      secondary: '#42E8B4',
+      text: '#000',
+      marked: '#012130'
     },
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["DM Sans Variable", "DM Sans", ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
         'hero': "url('../src/assets/banner_background_cta.png')",
       },
-      keyframes: 
-        {
-          pulseloop : {
-            '0%': { transform: 'scale(1) translateZ(0)', opacity: '.15' },
-            '30%': { opacity: '.15' },
-            '60%': { opacity: '0' },
-            '80%': { transform: 'scale(1.8) translateZ(0)', opacity: '0' },
-          }
-        }
+      lineHeight: {
+        '100': '100%',
+        '110': '110%',
+      },
+      keyframes:
+      {
+        pulseloop: {
+          '0%': { transform: 'scale(1) translateZ(0)', opacity: '.15' },
+          '30%': { opacity: '.15' },
+          '60%': { opacity: '0' },
+          '80%': { transform: 'scale(1.8) translateZ(0)', opacity: '0' },
+        },
+
+      },
     },
   },
   plugins: [
